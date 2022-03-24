8937c4a4aa1214638c473420a0221054b152e6b3
jcnpereira <casimiro@ipt.pt>
2022-03-24 22:49:29 +0000

    adição de migração

---
+++ Veterinarios/Veterinarios/Data/Migrations/20220324222942_CriaBaseDados.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Veterinarios.Data.Migrations
+{
+    public partial class CriaBaseDados : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Donos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sexo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NIF = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Veterinarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumCedulaProf = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fotografia = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veterinarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animais",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Especie = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Raca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Peso = table.Column<double>(type: "float", nullable: false),
+                    Foto = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DonoFK = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animais", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Animais_Donos_DonoFK",
+                        column: x => x.DonoFK,
+                        principalTable: "Donos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consultas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ValorConsulta = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Observacoes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AnimalFK = table.Column<int>(type: "int", nullable: false),
+                    VeterinarioFK = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consultas_Animais_AnimalFK",
+                        column: x => x.AnimalFK,
+                        principalTable: "Animais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Consultas_Veterinarios_VeterinarioFK",
+                        column: x => x.VeterinarioFK,
+                        principalTable: "Veterinarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animais_DonoFK",
+                table: "Animais",
+                column: "DonoFK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_AnimalFK",
+                table: "Consultas",
+                column: "AnimalFK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_VeterinarioFK",
+                table: "Consultas",
+                column: "VeterinarioFK");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consultas");
+
+            migrationBuilder.DropTable(
+                name: "Animais");
+
+            migrationBuilder.DropTable(
+                name: "Veterinarios");
+
+            migrationBuilder.DropTable(
+                name: "Donos");
+        }
+    }
+}
