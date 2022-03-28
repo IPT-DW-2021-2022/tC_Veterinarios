56ff055e46484a3c8615c1085ec89ee6f59a6bc6
jcnpereira <casimiro@ipt.pt>
2022-03-28 22:09:17 +0100

    adição de anotadores para condicionar o Modelo

---
+++ Veterinarios/Veterinarios/Data/Migrations/20220328191713_AddEmailDono.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Veterinarios.Data.Migrations
+{
+    public partial class AddEmailDono : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Donos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Donos");
+        }
+    }
+}
--- Veterinarios/Veterinarios/Models/Donos.cs
+++ Veterinarios/Veterinarios/Models/Donos.cs
@@ -1,4 +1,6 @@
-﻿namespace Veterinarios.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Veterinarios.Models {
 
    /// <summary>
    /// descrição dos dados dos clientes (donos do animais) da clínica
@@ -14,25 +16,43 @@
       /// <summary>
       /// PK para o dono
       /// </summary>
+      [Key]
       public int Id { get; set; }
 
       /// <summary>
       /// Nome do dono do animal
       /// </summary>
+      [Required(ErrorMessage = "o {0} é de preenchimento obrigatório")]
+      [StringLength(50, ErrorMessage = "o {0} não pode ter mais do que {1} caracteres.")]
       public string Nome { get; set; }
 
       /// <summary>
       /// sexo do dono do animal
       /// Mm - masculino; Ff - feminino
       /// </summary>
+      [Required(ErrorMessage = "o {0} é de preenchimento obrigatório")]
+      [StringLength(1, ErrorMessage = "o {0} não pode ter mais do que {1} carácter.")]
+      [RegularExpression("[MmFf]", ErrorMessage ="só pode escrever F para feminino ou M para masculino")]
       public string Sexo { get; set; }
 
       /// <summary>
       /// NIF do cliente
       /// </summary>
+      [Required(ErrorMessage = "o {0} é de preenchimento obrigatório")]
+      [StringLength(9, MinimumLength = 9, ErrorMessage = "o {0} deve ter exatamente {1} caracteres.")]
+      [RegularExpression("[0-9]{9}")]
       public string NIF { get; set; }
 
-      // lista dos animais do dono
+      /// <summary>
+      /// Email do dono do animal
+      /// </summary>
+      [EmailAddress(ErrorMessage = "Deve escrever um {0} válido.")]
+      public string Email { get; set; }
+
+
+      /// <summary>
+      /// lista dos animais do dono
+      /// </summary>
       public ICollection<Animais> ListaAnimais { get; set; }
 
 
