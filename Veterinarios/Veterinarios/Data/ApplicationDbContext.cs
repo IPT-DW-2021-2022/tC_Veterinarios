fe7cbda9e8ba1e06ec76739ec4ab9eabfc0a6339
jcnpereira <casimiro@ipt.pt>
2022-04-07 23:20:16 +0100

    adicionar registos para inserção automática no modelo

--- Veterinarios/Veterinarios/Data/ApplicationDbContext.cs
+++ Veterinarios/Veterinarios/Data/ApplicationDbContext.cs
@@ -14,6 +14,26 @@ namespace Veterinarios.Data {
       }
 
 
+      protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+         /*
+          * esta instrução importa todas as tarefas associadas ao método
+          * qd foi definida na superclasse
+          * */
+         base.OnModelCreating(modelBuilder);
+
+         // inicializar os dados das tabelas da BD
+         modelBuilder.Entity<MedicosVeterinarios>().HasData(
+            new MedicosVeterinarios { Id=1, Nome="José Lopes", NumCedulaProf="Vet-8768", Fotografia="Jose.jpg"},
+            new MedicosVeterinarios { Id =2, Nome = "Maria dos Santos", NumCedulaProf = "Vet-2568", Fotografia = "Maria.jpg" },
+            new MedicosVeterinarios { Id =3, Nome = "Ricardo Gonçalo Silva", NumCedulaProf = "Vet-2344", Fotografia = "Ricardo.jpg" }
+            );
+
+
+      }
+
+
+
       // definir as tabelas
       public DbSet<Animais> Animais { get; set; }
       public DbSet<Donos> Donos { get; set; }
---
+++ Veterinarios/Veterinarios/Data/Migrations/20220407221626_AddRecords2Vets.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Veterinarios.Data.Migrations
+{
+    public partial class AddRecords2Vets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Sexo",
+                table: "Donos",
+                type: "nvarchar(1)",
+                maxLength: 1,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Donos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NIF",
+                table: "Donos",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Veterinarios",
+                columns: new[] { "Id", "Fotografia", "Nome", "NumCedulaProf" },
+                values: new object[] { 1, "Jose.jpg", "José Lopes", "Vet-8768" });
+
+            migrationBuilder.InsertData(
+                table: "Veterinarios",
+                columns: new[] { "Id", "Fotografia", "Nome", "NumCedulaProf" },
+                values: new object[] { 2, "Maria.jpg", "Maria dos Santos", "Vet-2568" });
+
+            migrationBuilder.InsertData(
+                table: "Veterinarios",
+                columns: new[] { "Id", "Fotografia", "Nome", "NumCedulaProf" },
+                values: new object[] { 3, "Ricardo.jpg", "Ricardo Gonçalo Silva", "Vet-2344" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Veterinarios",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Veterinarios",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Veterinarios",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Sexo",
+                table: "Donos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)",
+                oldMaxLength: 1);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Donos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NIF",
+                table: "Donos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(9)",
+                oldMaxLength: 9);
+        }
+    }
+}
