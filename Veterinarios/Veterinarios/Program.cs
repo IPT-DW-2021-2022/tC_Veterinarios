eac6bdb126612190503e1e41b592e6f24f44a16c
jcnpereira <casimiro@ipt.pt>
2022-05-05 23:09:46 +0100

    adição de autenticação

--- Veterinarios/Veterinarios/Program.cs
+++ Veterinarios/Veterinarios/Program.cs
@@ -11,8 +11,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => 
+    options.SignIn.RequireConfirmedAccount = true
+)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
 builder.Services.AddControllersWithViews();
 
 // permite criar 'variáveis de sessão' (funcionam como 'cookies')
